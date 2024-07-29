acfbadcd5ccb943dad1352083dcc806add5d456c
masterjino72 <59094289+masterjino72@users.noreply.github.com>
2024-07-29 08:59:04 -0600

    Update AppDatabase.java
    Eliminacion de Instance doble

--- app/src/main/java/com/marty/yummy/dbutilities/AppDatabase.java
+++ app/src/main/java/com/marty/yummy/dbutilities/AppDatabase.java
@@ -24,9 +24,6 @@ public abstract class AppDatabase extends RoomDatabase {
     public static AppDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
-                if (INSTANCE == null) {
-                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),AppDatabase.class,"yummy").fallbackToDestructiveMigration().allowMainThreadQueries().build();
-                }
             }
         }
         return INSTANCE;
